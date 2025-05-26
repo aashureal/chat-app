cd3d601a5eb9d4f431051fb2e21c4708468f71b0
Aashutosh Uikey <aashuuy09@gmail.com>
2025-05-26 21:17:01 +0530

    ui update

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const socketIo = require("socket.io");
 const server = require("http").createServer(app);
 const io = socketIo(server);
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index");
