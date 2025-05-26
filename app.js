cb41a4985f0622afc9207840759c8e72dc871803
Aashutosh Uikey <aashuuy09@gmail.com>
2025-05-26 22:24:28 +0530

    ui update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ io.on("connection", (socket) => {
 
   socket.on("chat message", (msg) => {
     console.log(msg);
-    io.emit("chat message", msg);
+    io.emit("chat message", msg, socket.id);
   });
 
   socket.on("disconnect", () => {
